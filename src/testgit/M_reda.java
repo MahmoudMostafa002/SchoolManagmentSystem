f5dbf208296e7c1f8b8f333eb6da73a1cbe66811
mohamed shabana <mohamed shabana@Mohamed>
2015-12-12 23:49:07 +0200

    mohamed reda class 2

---
+++ src/testgit/M_reda.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testgit;
+
+/**
+ *
+ * @author mohamed shabana
+ */
+public class M_reda {
+    public static void main(String[] args) {
+        System.out.println("my 2nd class");
+    }
+}
