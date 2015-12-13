cb20addcc9bb00f776d1779214a9c29840a9c102
toshiba <toshiba@fge>
2015-12-13 00:49:46 -0800

    Rania3commit

---
+++ src/testgit/Rania.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testgit;
+
+/**
+ *
+ * @author toshiba
+ */
+public class Rania {
+
+    public Rania() {
+        System.out.println("Rania");
+    }
+    
+}
