169f8fe4d543285a7f590d4b0f1531ffb83540ea
manal pc <manal pc@Manal>
2015-12-21 18:43:11 +0200

    Manal Commit

---
+++ SchoolManagmentSystem/src/schoolmanagmentsystem/Manal1.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package schoolmanagmentsystem;
+
+/**
+ *
+ * @author manal pc
+ */
+public class Manal1 {
+
+    public Manal1() {
+        System.out.println("yarb noskt 4oya b2a :(");
+    }
+    
+}
